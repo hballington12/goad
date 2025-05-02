0f1c763086918ee6c6ce327fc8a54f622dc3c30c
hballington12 <hballington12@gmail.com>
2025-05-02 13:17:12 +0200

    move helper functions for testing into separate module

--- tests/fixed_orientation_tests.rs
+++ tests/fixed_orientation_tests.rs
@@ -1,5 +1,3 @@
-use std::{fs::File, io::BufReader, path::Path};
-
 use goad::{
     bins::{self, BinningScheme},
     multiproblem::MultiProblem,
@@ -7,15 +5,13 @@ use goad::{
     problem::collect_mueller,
     settings,
 };
+use helpers::{compare_results, load_reference_mueller};
 use num_complex::Complex32;
 
-// Tolerance for comparing Mueller matrix elements
-const TOL: f32 = 1e1;
+pub mod helpers;
 
-#[test]
-fn hello_world() {
-    assert_eq!(2 + 2, 4);
-}
+// Tolerance for comparing Mueller matrix elements
+const TOL: f32 = 1e2;
 
 #[test]
 fn fixed_hex_30_30_30() {
@@ -67,53 +63,4 @@ fn fixed_hex_30_20_20() {
     let result = collect_mueller(&multiproblem.result.mueller);
     let reference = load_reference_mueller("fixed_hex_30_20_20_mueller_scatgrid").unwrap();
     compare_results(result, reference, TOL).unwrap();
-}
-
-fn load_reference_mueller(filename: &str) -> Result<Vec<Vec<f32>>, Box<dyn std::error::Error>> {
-    let path = Path::new(env!("CARGO_MANIFEST_DIR"))
-        .join("tests")
-        .join("test_data")
-        .join(filename);
-
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
-    let mut data = Vec::new();
-
-    // Read the file line by line
-    for line in std::io::BufRead::lines(reader) {
-        let line = line?;
-
-        // Skip empty lines
-        if line.trim().is_empty() {
-            continue;
-        }
-
-        // Parse each value in the line, skipping the first 2 values
-        let row: Vec<f32> = line
-            .split_whitespace()
-            .skip(2) // Skip theta, phi
-            .filter_map(|s| s.parse::<f32>().ok())
-            .collect();
-
-        // Add the row to our data
-        if !row.is_empty() {
-            data.push(row);
-        }
-    }
-
-    Ok(data)
-}
-
-fn compare_results(
-    result: Vec<Vec<f32>>,
-    reference: Vec<Vec<f32>>,
-    tolerance: f32,
-) -> Result<(), Box<dyn std::error::Error>> {
-    for (r, ref_) in result.iter().zip(reference.iter()) {
-        for (a, b) in r.iter().zip(ref_.iter()) {
-            assert!((a - b).abs() < tolerance, "a: {}, b: {}", a, b);
-        }
-    }
-
-    Ok(())
 }
