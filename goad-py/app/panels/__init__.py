462e555a11497f06ead9662de5fe6e88494d67fe
hballington12 <hballington12@gmail.com>
2025-07-05 21:43:47 +0200

    Clean up the python bindings, remove the old python app

--- goad-py/app/panels/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Import all panel modules
-"""
-from .properties_panel import PropertiesPanel
-from .outliner_panel import OutlinerPanel
-from .console_panel import ConsolePanel
-from .simulation_settings_panel import SimulationSettingsPanel
-
-__all__ = ['ControlPanel', 'PropertiesPanel', 'OutlinerPanel', 'ConsolePanel', 'SimulationSettingsPanel']
