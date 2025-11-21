413d614fec833500eef7583e1d0b68a20bb74f8a
hballington12 <hballington12@gmail.com>
2025-11-21 15:37:36 +0100

    clean up some examples from src

--- python/goad/convergence/albedo.py
+++ python/goad/convergence/albedo.py
@@ -25,18 +25,3 @@ class Albedo(Convergable):
     @property
     def sem(self) -> float | None:
         return self.tracker.sem
-
-
-if __name__ == "__main__":
-    from goad import Settings
-    from goad.convergence.base import Convergence
-
-    convergence = Convergence(
-        Settings(
-            geom_path="../../../examples/data/hex.obj",
-            quiet=True,
-            particle_refr_index_im=0.001,
-        ),
-        [Albedo(tolerance=Tolerance.RELATIVE, threshold=0.005)],
-    )
-    convergence.run()
--- python/goad/convergence/ext_cross.py
+++ python/goad/convergence/ext_cross.py
@@ -11,7 +11,6 @@ class ExtCross(Convergable):
 
     def update(self, result: Results) -> None:
         if result.ext_cross is None or np.isnan(result.ext_cross):
-            # print("INFO: Tried to update but scattering cross section is None or NaN")
             return
 
         self.i += 1
@@ -24,18 +23,3 @@ class ExtCross(Convergable):
     @property
     def sem(self) -> float | None:
         return self.tracker.sem
-
-
-if __name__ == "__main__":
-    from goad import Settings
-    from goad.convergence.base import Convergence
-
-    convergence = Convergence(
-        Settings(
-            geom_path="../../../examples/data/hex.obj",
-            quiet=True,
-            particle_refr_index_im=0.001,
-        ),
-        [ExtCross(tolerance=Tolerance.RELATIVE, threshold=0.02)],
-    )
-    convergence.run()
