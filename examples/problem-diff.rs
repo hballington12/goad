fee7d44d29ed0851ed5214aa6ff3460ea12f4d9d
Harry Ballington <hballington12@gmail.com>
2025-11-23 11:40:02 +0100

    small fix

--- examples/problem-diff.rs
+++ examples/problem-diff.rs
@@ -2,7 +2,8 @@ use goad::geom::{self};
 use goad::problem::Problem;
 
 fn main() {
-    let mut geom = geom::Geom::load("./examples/data/concave2.obj").unwrap();
+    let geoms = geom::Geom::load("./examples/data/concave2.obj").unwrap();
+    let mut geom = geoms[0].clone();
 
     geom.shapes[0].refr_index.re = 1.31;
     geom.shapes[0].refr_index.im = 0.001;
